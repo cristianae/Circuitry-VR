44ada6df30741848a460c9afa45a615f731b5631
Cristiana Eagen <Cristiana.M.Eagen-1@ou.edu>
2025-12-08 14:23:03 -0600

    added the logic for a battery fire

---
+++ Circuitry VR Project/Assets/BatteryFire.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryFire : MonoBehaviour
+{
+    [SerializeField] WiringPool wiringPool;
+    [SerializeField] GameObject fireEffect;
+    [SerializeField] GameObject positiveEnd;
+    [SerializeField] GameObject negativeEnd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool circuitComplete = IsConnected(positiveEnd, negativeEnd);
+        fireEffect.SetActive(circuitComplete);
+    }
+
+    bool IsConnected(GameObject start, GameObject end)
+    {
+        var connections = wiringPool.getWireConnections();
+        var visited =new HashSet<GameObject>();
+        var stack = new Stack<GameObject>();
+        stack.Push(start);
+        while (stack.Count> 0)
+        {
+           GameObject current = stack.Pop();
+           if (!visited.Add(current)) 
+               continue;
+           if (current == end)
+               return true;
+            foreach (var pair in connections)
+            {
+                if (pair[0] == current && !visited.Contains(pair[1]))
+                {
+                    stack.Push(pair[1]);
+                }
+                else if (pair[1] == current && !visited.Contains(pair[0]))
+                {
+                    stack.Push(pair[0]);
+                }
+            }
+        }
+
+        return false;
+    }
+}
